f6159361d6b961cfe06ea1a23de3705cace0b3db
chuuhyeseung <cheesehsmay5th@gmail.com>
2022-11-20 15:29:10 +0900

    Footer, Button 추가

--- react-prepare-01/helloapp/src/App.tsx
+++ react-prepare-01/helloapp/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CountryList from './CountryList';
+import styles from './styles'
+import AppCssModule from './App.module.css'
+import Footer from './Footer'
+import {
+    BasicButton, ItalicButton, UnderLineButton, WhiteUnderLineButton
+} from './Buttons'
 
-type Props = {}
+export type CountryType = {
+    no: number;
+    country: string;
+    visited: boolean;
+};
 
-const App = (props: Props) => {
-    let msg = "World";
+const App = () => {
+    const [msg, setMsg] = useState<string>("World");
+    const [list, setList] = useState<Array<CountryType>>([
+        { no: 1, country: "이집트", visited: false },
+        { no: 2, country: "일본", visited: true },
+        { no: 3, country: "미국", visited: false },
+        { no: 4, country: "콜롬비아", visited: false },
+    ]);
+    const [theme, setTheme] = useState<string>("basic");
 
     const addResult = (x: number, y:number) => {
         return (
@@ -17,15 +34,20 @@ const App = (props: Props) => {
     };
 
     return (
-        <div className="container">
-            <div> 
-                <h2> Hello { msg } !!</h2>
-                <hr />
+        <>
+            <div className="container">
+                <h2 className={AppCssModule.test}> Hello { msg } !!</h2>
+                <hr style={styles.dashStyle} />
                 {addResult(4, 3)}
                 <CountryList countries={list}/> 
+                <BasicButton>기본</BasicButton>
+                <ItalicButton>이탤릭</ItalicButton>
+                <UnderLineButton>언더라인</UnderLineButton>
+                <WhiteUnderLineButton>화이트 언더라인</WhiteUnderLineButton>
+                <Footer theme={theme} />
             </div>
-        </div>
+        </>
     )
 }
 
-export default App
+export default App;
